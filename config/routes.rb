5c9de7d039e6640b8ebc731ad6fa4ea8cf407d66
vader1359 <vader1359@gmail.com>
2017-08-09 17:16:13 +0700

    FInish milestone 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-
+  
+  post 'friendship/create' => "friendships#create"
+  post 'friendship/destroy' => "friendships#destroy"
+  
   get "login" => "sessions#new"
   post "login" => "sessions#create"
   get "logout" => "sessions#destroy"
   resources :users
-
   root 'home#index'
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
