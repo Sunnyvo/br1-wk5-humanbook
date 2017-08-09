1ee41eae4af2ee7bbb0b7494e3e38aa9b5b6e87e
vader1359 <vader1359@gmail.com>
2017-08-09 14:23:50 +0700

    First commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
-
+  
   private
 
   def login(user)
@@ -16,3 +16,11 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(id: session[:user_id])
   end
 end
+ def require_login
+  unless current_user 
+    redirect_to root_path
+    flash[:error] = "Access Demied. Please login to view"   
+  end
+    
+
+ end
